132c25610cc781f00987a397366c89e1941b1281
nemo <kalleilv@gmail.com>
2020-02-24 14:13:36 +0200

    Basic tests & ci workflow

---
+++ src/graphql/__tests__/schema.test.js
@@ -0,0 +1,9 @@
+import createSchema from '../schema';
+
+describe('createSchema', () => {
+  it('creates schema without errors', () => {
+    createSchema();
+
+    expect(true).toBe(true);
+  });
+});
